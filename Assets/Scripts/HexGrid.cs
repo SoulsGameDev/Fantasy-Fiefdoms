89126f22f00b8dab323c98d5db1b1799ccd12c67
Soulmender <p.znuderl@gmail.com>
2023-07-14 14:50:59 +0200

    Video 2 - Mesh Generation
    Added a method to generate the mesh

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -11,7 +11,63 @@ public class HexGrid : MonoBehaviour
     [field:SerializeField] public int Height { get; private set; }
     [field:SerializeField] public float HexSize { get; private set; }
     [field:SerializeField] public GameObject HexPrefab { get; private set; }
+
+    private void Start()
+    {
+        GenerateMesh();
+    }
+
     //TODO: Create a grid of hexes
+    private void GenerateMesh()
+    {
+        Vector3[] vertices = new Vector3[7 * Width * Height];
+
+        for (int z = 0; z < Height; z++)
+        {
+            for (int x = 0; x < Width; x++)
+            {
+                Vector3 centrePosition = HexMetrics.Center(HexSize, x, z, Orientation) + transform.position;
+                vertices[(z * Width + x) * 7] = centrePosition;
+                for (int s = 0; s < HexMetrics.Corners(HexSize, Orientation).Length; s++)
+                {
+                    vertices[(z * Width + x) * 7 + s + 1] = centrePosition + HexMetrics.Corners(HexSize, Orientation)[s % 6];
+                }
+            }
+        }
+
+        int[] triangles = new int[3 * 6 * Width * Height];
+        for (int z = 0; z < Height; z++)
+        {
+            for (int x = 0; x < Width; x++)
+            {
+                for (int s = 0; s < HexMetrics.Corners(HexSize, Orientation).Length; s++)
+                {
+                    int cornerIndex = s + 2 > 6 ? s + 2 - 6 : s + 2;
+                    triangles[3 * 6 * (z * Width + x) + s * 3 + 0] = (z * Width + x) * 7;
+                    triangles[3 * 6 * (z * Width + x) + s * 3 + 1] = (z * Width + x) * 7 + s + 1;
+                    triangles[3 * 6 * (z * Width + x) + s * 3 + 2] = (z * Width + x) * 7 + cornerIndex;
+                    
+                    
+                }
+            }
+        }
+
+
+        Mesh mesh = new Mesh();
+        mesh.name = "Hex Mesh";
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+        mesh.Optimize();
+        mesh.RecalculateUVDistributionMetrics();
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        if (meshFilter == null)
+        {
+            meshFilter = gameObject.AddComponent<MeshFilter>();
+        }
+        meshFilter.mesh = mesh;
+    }
     //TODO: Store the individual tiles in an array
     //TODO: Methods to get, change, add , and remove tiles
 
