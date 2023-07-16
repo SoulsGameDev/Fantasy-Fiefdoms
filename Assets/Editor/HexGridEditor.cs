1195fadbb8479890a0b61a53a5d3b0667394fd65
Soulmender <p.znuderl@gmail.com>
2023-07-16 14:40:18 +0200

    Created Editor for HexGrid

---
+++ Assets/Editor/HexGridEditor.cs
@@ -0,0 +1,26 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(HexGrid))]
+public class HexGridEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        HexGrid hexGrid = (HexGrid)target;
+
+        for (int z = 0; z < hexGrid.Height; z++)
+        {
+            for (int x = 0; x < hexGrid.Width; x++)
+            {
+                Vector3 centrePosition = HexMetrics.Center(hexGrid.HexSize, x, z, hexGrid.Orientation) + hexGrid.transform.position;
+
+                int centerX = x;//- hexGrid.Width / 2 + x;
+                int centerZ = z;//- hexGrid.Height / 2 + z;
+                // Show the coordinates in a label
+                Vector3 cubeCoord = HexMetrics.OffsetToCube(centerX, centerZ, hexGrid.Orientation);
+                Handles.Label(centrePosition + Vector3.forward*0.5f, $"[{centerX}, {centerZ}]");
+                Handles.Label(centrePosition, $"({cubeCoord.x}, {cubeCoord.y}, {cubeCoord.z})");
+            }
+        }
+    }
+}
