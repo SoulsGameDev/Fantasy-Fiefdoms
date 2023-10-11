f8382340895a5089d03414f4891195d9a741bc27
Soulmender <p.znuderl@gmail.com>
2023-10-11 12:31:11 +0200

    Added Orbit Camera With Time
    Added script which orbits the camera through time rather than with mouse change.
    It also extends inspector functionality so that if we change the maxspeed on orbital transposer the rotation speed change gets reflected in the OrbitCameraWithTime script. Added for easier testing of rotation speed value.

---
+++ Assets/Scripts/OrbitCameraWithTime.cs
@@ -0,0 +1,99 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class OrbitCameraWithTime : MonoBehaviour
+{
+    [SerializeField]
+    CinemachineVirtualCamera orbitalCamera;
+    CinemachineOrbitalTransposer orbitalTransposer;
+    float rotationSpeed = 10f;
+
+
+    Coroutine rotationCoroutine;
+
+    private void Awake()
+    {
+        if(orbitalCamera == null)
+        {
+            orbitalCamera = GetComponent<CinemachineVirtualCamera>();
+        }
+
+        if(orbitalCamera == null)
+        {
+            Debug.LogError("No orbital camera found");
+        }
+
+        orbitalTransposer = orbitalCamera.GetCinemachineComponent<CinemachineOrbitalTransposer>();
+
+        if(orbitalTransposer == null)
+        {
+            Debug.LogError("No orbital transposer found");
+        }
+
+        rotationSpeed = orbitalTransposer.m_XAxis.m_MaxSpeed;
+
+        CameraController.Instance.onCameraChanged += OnCameraChange;
+    }
+
+    private void OnCameraChange(CinemachineVirtualCamera camera)
+    {
+        if(camera != orbitalCamera)
+        {
+            return;
+        }
+        CinemachineOrbitalTransposer newTransposer = camera.GetCinemachineComponent<CinemachineOrbitalTransposer>();
+        if(orbitalTransposer == newTransposer)
+        {
+            if(rotationCoroutine != null)
+            {
+                Debug.Log("Stop rotation");
+                StopCoroutine(rotationCoroutine);
+            }
+            Debug.Log("Start rotation");
+            rotationCoroutine = StartCoroutine(Rotate());
+        }
+        else
+        {
+            Debug.Log("Stop rotation");
+            StopCoroutine(rotationCoroutine);
+        }
+    }
+
+    public IEnumerator Rotate()
+    {
+        while (true)
+        {
+            orbitalTransposer.m_XAxis.Value +=  rotationSpeed * Time.deltaTime;
+            yield return null;
+        }
+    }
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(OrbitCameraWithTime))]
+    public class OrbitCameraEditor : Editor
+    {
+        private float prevMaxSpeed;
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            OrbitCameraWithTime orbitCameraScript = (OrbitCameraWithTime)target;
+
+            if (Application.isPlaying)
+            {
+                // Check if the maxSpeed has changed
+                if (orbitCameraScript.orbitalTransposer != null &&
+                    orbitCameraScript.orbitalTransposer.m_XAxis.m_MaxSpeed != prevMaxSpeed)
+                {
+                    orbitCameraScript.rotationSpeed = orbitCameraScript.orbitalTransposer.m_XAxis.m_MaxSpeed;
+                    prevMaxSpeed = orbitCameraScript.orbitalTransposer.m_XAxis.m_MaxSpeed;
+                }
+            }
+        }
+    }
+#endif
+}
