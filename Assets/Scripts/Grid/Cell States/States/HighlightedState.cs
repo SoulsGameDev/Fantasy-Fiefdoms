50f6f832fef1fcd2aaae1cc9c2d88e6a68b0fa45
Soulmender <p.znuderl@gmail.com>
2023-11-03 11:54:09 +0100

    LeanTween Import and Highlight state update
    Imported LeanTween and updated HighlighedState so that it rases and scales the tile that is highlighted and resets position on exiting the state.

--- Assets/Scripts/Grid/Cell States/States/HighlightedState.cs
+++ Assets/Scripts/Grid/Cell States/States/HighlightedState.cs
@@ -7,12 +7,17 @@ public class HighlightedState : BaseCellState
 
     public override void Enter(HexCell cell)
     {
-        
+        //Tween local Scale of the cell.Terrain gameobject
+        LeanTween.scale(cell.Terrain.gameObject, Vector3.one * 1.2f, 0.2f).setEase(LeanTweenType.easeOutBack);
+        LeanTween.moveY(cell.Terrain.gameObject, 5f, 0.2f).setEase(LeanTweenType.easeOutBack);
     }
 
     public override void Exit(HexCell cell)
     {
         Debug.Log($"Cell {cell.AxialCoordinates} is exiting Highlighted State");
+        //Tween local Scale of the cell.Terrain gameobject
+        LeanTween.scale(cell.Terrain.gameObject, Vector3.one, 0.2f).setEase(LeanTweenType.easeOutBack);
+        LeanTween.moveY(cell.Terrain.gameObject, 0f, 0.2f).setEase(LeanTweenType.easeOutBack);
     }
 
     public override ICellState OnMouseExit()
